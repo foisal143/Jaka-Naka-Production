b03d47cf10577b17235cd3018138a6d557f1c2fd
Md foisal islam~ <islammdfoisal54@gmail.com>
2023-11-09 06:59:58 -0800

    nav scroll done

--- hello.js
+++ hello.js
@@ -10,3 +10,17 @@ close.addEventListener('click', () => {
   responsiveNav.style.left = '-100vw';
   ope.style.visibility = 'visible';
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById('navbar').style.backgroundColor = 'white';
+    document.getElementById('navbar').style.color = 'black';
+  } else {
+    document.getElementById('navbar').style.backgroundColor = 'transparent';
+    document.getElementById('navbar').style.color = 'white';
+  }
+}
